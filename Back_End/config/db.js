c2f4c65abb8150ce73b7e3cf69c98441830397ef
thien1 <thien3@gmail.com>
2024-11-15 12:02:18 +0700

    thien

--- Back_End/config/db.js
+++ Back_End/config/db.js
@@ -5,13 +5,9 @@ const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-<<<<<<< HEAD
     port: 3307,
     database: 'datn'
-=======
-    port: 3306,
-    database: 'datn2'
->>>>>>> efb196e8958326bcab0d39305e77de6a2642a86f
+
 });
 db.connect(err => {
     if (err) throw err;
