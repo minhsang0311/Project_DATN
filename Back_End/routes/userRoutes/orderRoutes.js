f0d9de7c3e75f8e21b9392ba0c1d51378bfd374c
Nguyen-hiep12 <Nguyenhiep12062004@gmail.com>
2024-12-20 01:46:22 +0700

    hahaa

--- Back_End/routes/userRoutes/orderRoutes.js
+++ Back_End/routes/userRoutes/orderRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const orderController = require('../../contrllers/userControllers/orderController'); // Fixed 'controllers' spelling
 const { userMiddleware } = require('../../middlewares/userMiddlware');
 // Route to get orders based on user ID
-router.get('/orders/:userId',userMiddleware, orderController.getOrderList);
+router.get('/orders/:userId', orderController.getOrderList);
 router.get('/orderDetail/:orderDetailId',userMiddleware, orderController.getOrderDetailById);
 router.put('/cancelOrder/:orderId', userMiddleware, orderController.putcancelOrder);
 
